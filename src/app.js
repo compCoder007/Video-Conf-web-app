a7e9661e606bc986d3840591461e25d1be6cc32a
Kotamraju Umesh Chandra <112859159+compCoder007@users.noreply.github.com>
2024-11-14 15:52:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const fs=require('fs');
 const path = require( 'path' );
 
 const options = {
-    key:fs.readFileSync(path.join(__dirname,'../../certs/key.pem')),
-    cert:fs.readFileSync(path.join(__dirname,'../../certs/cert.pem'))
+    key:fs.readFileSync(path.join(__dirname,'../key.pem')),
+    cert:fs.readFileSync(path.join(__dirname,'../cert.pem'))
 }
 
 const server = require( 'https' ).createServer(options,app);
